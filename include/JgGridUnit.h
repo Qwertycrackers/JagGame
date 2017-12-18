51f96c93b09823a96e97af3f3ae96f5ab3ad31c6
Joseph A. Gerardot <indyJAG@gmail.com>
2017-12-18 01:47:05 -0500

    Add more to GridUnit.
    Should be good for now, unless I missed something obvious.

--- include/JgGridUnit.h
+++ include/JgGridUnit.h
@@ -16,13 +16,15 @@ class GridUnit: public Unit {
 	~GridUnit(); //!<\brief Destructor
 	GridUnit(SDL_Renderer*); //!<\brief Quickly set the renderer in constructor.
 	Uint32 getEventFlags(); //!<\brief Get the GridUnit event flags. It normally needs KeyEvents and MouseEvents.
+	void setTileDataEngine(TileDataEngine *e); //!<\brief Set the `TileDataEngine` with which this `GridUnit` is to interface.
 	void render(); //!<\brief Render this `Unit` to its region.
 	void setRegion(SDL_Rect *r); //!<\brief Overloaded `setRegion()`.
 	protected:
 	void handleEvent(SDL_Event&); //!<\brief Handle a single event.
 	inline Cell findMouseCell(SDL_Event&); //!<\brief Find the cell coordinates of a mouse event.
-	inline void handleKeyInput(SDL_Event&) //!<\brief Handle a key event for input.
+	inline void handleKeyInput(SDL_Event&) //!<\brief Handle a key input.
 	inline void updateTileWidth(); //!<\brief Change tile width in response to various things
+	Cell *origin; //!<\brief The top-left cell on the screen.
 	TileDataEngine *engine; //!<\brief The engine handling game logic for this `Unit`.
 	int tileWidth; //!<\brief Width of a tile at the current moment
 	int numTiles; //!<\brief Number of tiles on a side at the current moment
--- src/JgGridUnit.cpp
+++ src/JgGridUnit.cpp
@@ -7,16 +7,23 @@ GridUnit(SDL_Renderer *r): Unit(r) {}
 ~GridUnit() {}
 /*! \returns A bitwise OR'd flag representing the `SDL_Event` types accepted. */
 Uint32 GridUnit()::getEventFlags() {
-	return SDL_KEYDOWN|SDL_MOUSEMOTION|SDL_MOUSEBUTTON;
+	return SDL_KEYDOWN|SDL_MOUSEMOTION|SDL_MOUSEBUTTON; //perhaps do more logic here later
 }
 /*! Set the region occupied by this `GridUnit`. 
 	\param r the region to be used. */
 void GridUnit::setRegion(SDL_Rect *r) {
 	Unit::setRegion(r);
-	updateTileWidth;
+	updateTileWidth();
 }
-void updateTileWidth() {
-	tileWidth = ((region->x > region->y) ? region->y : region->x)/numUnits;
+/*! Update the onscreen tile width in response to something. */
+void GridUnit::updateTileWidth() {
+	tileWidth = ((region->w > region->l) ? region->l : region->w)/numTiles;
+}
+/*! Set the `TileDataEngine`.
+ * \param e Pointer to the engine to use */
+void GridUnit::setTileDataEngine(TileDataEngine *e) {
+	engine = e;
+	origin = engine->getOrigin();
 }
 /*! \param *events A pointer to a `std::vector` containing the events to handle. */
 void GridUnit::handleEvent(SDL_Event &e) {
@@ -27,7 +34,44 @@ void GridUnit::handleEvent(SDL_Event &e) {
 	if(e.type == SDL_KEYDOWN)
 		handleKeyInput(e);
 }
-
+/*! Handle a single key input
+ * 	\param e The SDL_KeyEvent to handle */
+void GridUnit::handleKeyInput(SDL_Event &e) {
+	switch(e.key.keysym.sym) {
+		case(SDLK_RIGHTBRACKET):
+			numTiles += 3; //probably stop using magic numbers at some point
+			updateTileWidth();
+			break;
+		case(SDLK_LEFTBRACKET):
+			numTiles = (numTiles - 3 < 0) ? numTiles : numTiles - 3;
+			updateTileWidth();
+			break;
+	}
+	engine->handleKeyInput(e);
+}
+/*! Render a frame of data from the `TileDataEngine` onto the region prescribed to this `Unit`. */
 void GridUnit::render() {
-
+	int i = 0; //counter in the x
+	int j = 0; //counter in the y
+	SDL_Texture *sprite; //whatever's on the tile
+	SDL_Texture *floor; //the tile's backround
+	SDL_Rect tile; //rectangle where the current tile will go
+	tile.x = region.x; //set starting x and y to upper corner of region.
+	tile.y = region.y;
+	tile.h = tileWidth; //make area as large as one tile (always square)
+	tile.w = tileWidth;
+	while(tile.x < region->x + region->w) { //until we've looped across the x
+		while(tile.y < region->y + region->h) { //until we've looped across the y
+			engine->getTextures(origin,i,j,sprite,floor); //Set sprite and floor to point at the textures for the cell
+			SDL_RenderCopy(renderer, floor, NULL, tile); //render the floor
+			SDL_RenderCopy(renderer, sprite, NULL, tile); //render the sprite
+			j++;
+			tile.y += tileWidth;
+		}
+		j = 0;
+		i++;
+		tile.x += tileWidth;
+	}
+}
+		
 }
